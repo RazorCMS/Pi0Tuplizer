c47ca035c26bc73392bb422bd50c4053517bc761
Zhicai <zzhang2@caltech.edu>
2017-03-27 22:36:08 +0200

    remove junk files

--- python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/PiZero/Pi0Tuplizer/',1)[0])+'/cfipython/slc6_amd64_gcc530/PiZero/Pi0Tuplizer')
