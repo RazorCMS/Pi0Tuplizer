3273d9c7825f5b49ce5f56b197f0fe17b606e0a5
Zhicai <zzhang2@caltech.edu>
2017-04-19 14:21:31 +0200

    update cfg file for GEN_SIM_RECO

--- python/Pi0Tuplizer_830_GEN_SIM_RECO.py
+++ python/Pi0Tuplizer_830_GEN_SIM_RECO.py
@@ -1,6 +1,5 @@
 #########################options##############################
 isMC_ = True
-isPi0_ = True
 FillL1SeedFinalDecision_ = True
 FillDiPhotonNtuple_ = True
 FillPhotonNtuple_ = True
@@ -29,9 +28,9 @@ process.load("DQMOffline.Configuration.DQMOfflineMC_cff")
 
 process.GlobalTag.globaltag = '81X_upgrade2017_realistic_v26'
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 
-process.MessageLogger.cerr.FwkReport.reportEvery = 10
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 process.options = cms.untracked.PSet(
    wantSummary = cms.untracked.bool(True),
    SkipEvent = cms.untracked.vstring('ProductNotFound','CrystalIDError')
@@ -54,32 +53,57 @@ process.TFileService = cms.Service("TFileService",
 process.ntuples = cms.EDAnalyzer('Pi0Tuplizer',
 FillL1SeedFinalDecision = cms.untracked.bool(True),
 uGtAlgInputTag = cms.untracked.InputTag('hltGtStage2Digis'),
-EBRecHitCollectionTag = cms.untracked.InputTag("ecalRecHit","EcalRecHitsEB","RECO"),
-EERecHitCollectionTag = cms.untracked.InputTag("ecalRecHit","EcalRecHitsEE","RECO"),
-ESRecHitCollectionTag = cms.untracked.InputTag("ecalPreshowerRecHit","EcalRecHitsES","RECO"),
+EBRecHitCollectionTag_Pi0 = cms.untracked.InputTag("ecalRecHit","EcalRecHitsEB","RECO"),
+EERecHitCollectionTag_Pi0 = cms.untracked.InputTag("ecalRecHit","EcalRecHitsEE","RECO"),
+ESRecHitCollectionTag_Pi0 = cms.untracked.InputTag("ecalPreshowerRecHit","EcalRecHitsES","RECO"),
+EBRecHitCollectionTag_Eta = cms.untracked.InputTag("ecalRecHit","EcalRecHitsEB","RECO"),
+EERecHitCollectionTag_Eta = cms.untracked.InputTag("ecalRecHit","EcalRecHitsEE","RECO"),
+ESRecHitCollectionTag_Eta = cms.untracked.InputTag("ecalPreshowerRecHit","EcalRecHitsES","RECO"),
 PhotonOrderOption = cms.untracked.string("SeedEBased"),# "SeedEBased" (g1 is the one with larger seed Energy) or "PhoPtBased"(g1 is the one with larger pt)
-EB_Seed_E = cms.untracked.double(0.5),
-EE_Seed_E = cms.untracked.double(0.5),
-pi0PtCut_barrel1 = cms.untracked.double(2.6),
-pi0PtCut_barrel2 = cms.untracked.double(2.6),
-pi0PtCut_endcap1 = cms.untracked.double(3.0),
-pi0PtCut_endcap2 = cms.untracked.double(1.5),
-gPtCut_barrel1 = cms.untracked.double(1.3),
-gPtCut_barrel2 = cms.untracked.double(1.3),
-gPtCut_endcap1 = cms.untracked.double(0.95),
-gPtCut_endcap2 = cms.untracked.double(0.65),
-s4s9Cut_barrel1 = cms.untracked.double(0.83),
-s4s9Cut_barrel2 = cms.untracked.double(0.83),
-s4s9Cut_endcap1 = cms.untracked.double(0.95),
-s4s9Cut_endcap2 = cms.untracked.double(0.95),
-nxtal1Cut_barrel1 = cms.untracked.double(0.),
-nxtal1Cut_barrel2 = cms.untracked.double(0.),
-nxtal1Cut_endcap1 = cms.untracked.double(0.),
-nxtal1Cut_endcap2 = cms.untracked.double(0.),
-nxtal2Cut_barrel1 = cms.untracked.double(0.),
-nxtal2Cut_barrel2 = cms.untracked.double(0.),
-nxtal2Cut_endcap1 = cms.untracked.double(0.),
-nxtal2Cut_endcap2 = cms.untracked.double(0.)
+EB_Seed_E_Pi0_ = cms.untracked.double(0.5),
+EE_Seed_E_Pi0_ = cms.untracked.double(0.5),
+pairPtCut_barrel1_Pi0_ = cms.untracked.double(2.6),
+pairPtCut_barrel2_Pi0_ = cms.untracked.double(2.6),
+pairPtCut_endcap1_Pi0_ = cms.untracked.double(3.0),
+pairPtCut_endcap2_Pi0_ = cms.untracked.double(1.5),
+gPtCut_barrel1_Pi0_ = cms.untracked.double(1.3),
+gPtCut_barrel2_Pi0_ = cms.untracked.double(1.3),
+gPtCut_endcap1_Pi0_ = cms.untracked.double(0.95),
+gPtCut_endcap2_Pi0_ = cms.untracked.double(0.65),
+s4s9Cut_barrel1_Pi0_ = cms.untracked.double(0.83),
+s4s9Cut_barrel2_Pi0_ = cms.untracked.double(0.83),
+s4s9Cut_endcap1_Pi0_ = cms.untracked.double(0.95),
+s4s9Cut_endcap2_Pi0_ = cms.untracked.double(0.95),
+nxtal1Cut_barrel1_Pi0_ = cms.untracked.double(0.),
+nxtal1Cut_barrel2_Pi0_ = cms.untracked.double(0.),
+nxtal1Cut_endcap1_Pi0_ = cms.untracked.double(0.),
+nxtal1Cut_endcap2_Pi0_ = cms.untracked.double(0.),
+nxtal2Cut_barrel1_Pi0_ = cms.untracked.double(0.),
+nxtal2Cut_barrel2_Pi0_ = cms.untracked.double(0.),
+nxtal2Cut_endcap1_Pi0_ = cms.untracked.double(0.),
+nxtal2Cut_endcap2_Pi0_ = cms.untracked.double(0.),
+EB_Seed_E_Eta_ = cms.untracked.double(0.5),
+EE_Seed_E_Eta_ = cms.untracked.double(0.5),
+pairPtCut_barrel1_Eta_ = cms.untracked.double(4.0),
+pairPtCut_barrel2_Eta_ = cms.untracked.double(4.0),
+pairPtCut_endcap1_Eta_ = cms.untracked.double(3.0),
+pairPtCut_endcap2_Eta_ = cms.untracked.double(3.0),
+gPtCut_barrel1_Eta_ = cms.untracked.double(1.2),
+gPtCut_barrel2_Eta_ = cms.untracked.double(1.2),
+gPtCut_endcap1_Eta_ = cms.untracked.double(1.0),
+gPtCut_endcap2_Eta_ = cms.untracked.double(0.70),
+s4s9Cut_barrel1_Eta_ = cms.untracked.double(0.87),
+s4s9Cut_barrel2_Eta_ = cms.untracked.double(0.87),
+s4s9Cut_endcap1_Eta_ = cms.untracked.double(0.90),
+s4s9Cut_endcap2_Eta_ = cms.untracked.double(0.90),
+nxtal1Cut_barrel1_Eta_ = cms.untracked.double(0.),
+nxtal1Cut_barrel2_Eta_ = cms.untracked.double(0.),
+nxtal1Cut_endcap1_Eta_ = cms.untracked.double(0.),
+nxtal1Cut_endcap2_Eta_ = cms.untracked.double(0.),
+nxtal2Cut_barrel1_Eta_ = cms.untracked.double(0.),
+nxtal2Cut_barrel2_Eta_ = cms.untracked.double(0.),
+nxtal2Cut_endcap1_Eta_ = cms.untracked.double(0.),
+nxtal2Cut_endcap2_Eta_ = cms.untracked.double(0.)
 )
 
 #########################paratmeters for the tuplizer##############################
@@ -87,10 +111,6 @@ if isMC_:
         process.ntuples.isMC = cms.untracked.bool(True)
 else:
         process.ntuples.isMC = cms.untracked.bool(False)
-if isPi0_:
-        process.ntuples.isPi0 = cms.untracked.bool(True)
-else:
-        process.ntuples.isPi0 = cms.untracked.bool(False)
 if FillL1SeedFinalDecision_:
         process.ntuples.FillL1SeedFinalDecision = cms.untracked.bool(True)
 else:
@@ -106,6 +126,4 @@ else:
 
 #define path
 process.p = cms.Path()
-#process.p *= process.ecalDigis*process.ecalPreshowerDigis
-#process.p *= process.bunchSpacingProducer*process.ecalLocalRecoSequence
 process.p *= process.ntuples
